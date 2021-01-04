d5f808d9549d3a269f5e55ffe12862ac83839b91
Md Zaman <md83shahnewaz@gmail.com>
2021-01-03 20:19:29 -0500

    Refactoring code

--- src/components/api/tabs/appium.js
+++ src/components/api/tabs/appium.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import GoogleAd from '../../GoogleAd';
+import link from '../links.json';
 import appiumLogo from '../../../images/tabs/appiumLogo.png'; 
 
-const GITHUB_ICON ='http://www.protractortest.org/img/GitHub-Mark-Light-32px.png';
-const APPIUM_GITHUB = 'https://github.com/appium/appium';
-const APPIUM_WEBSITE = 'http://appium.io/';
-
-const resAppium = () => {
+const resAppium = ({path}) => {
     return(
         <div className='page-tab-body'>
             <div>
@@ -16,16 +13,15 @@ const resAppium = () => {
                 <img className='appium-logo' src={appiumLogo} alt='appium Logo'/>
             </div>
             <div className='tc pt4'>
-                <a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-red' href={APPIUM_GITHUB} target="_blank" rel="noopener noreferrer">
-                    <img className='pr2' src={GITHUB_ICON} alt='github log' /> 
+                <a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-red' href={link.APPIUM_WEBSITE} target="_blank" rel="noopener noreferrer">
+                    <img className='pr2' src={link.GITHUB_ICON} alt='github log' /> 
                     Appium on GitHub                 
                 </a>
-                <a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-green' href={APPIUM_WEBSITE} target="_blank" rel="noopener noreferrer">
-                    <img className='pr2' src={GITHUB_ICON} alt='github log' /> 
+                <a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-green' href={link.APPIUM_WEBSITE} target="_blank" rel="noopener noreferrer">
+                    <img className='pr2' src={link.GITHUB_ICON} alt='github log' /> 
                     Appium Org                 
                 </a>
             </div>
-
 
             <GoogleAd slot="1541085932" classNames="page-right-side" />
         </div>
---
+++ src/utilities/utils.js
@@ -0,0 +1,13 @@
+export const path = [
+    {
+        appiumLogo: '../../../images/tabs/appiumLogo.png'
+
+    },
+    {
+        sLogo: '../../../images/tabs/appiumLogo.png'
+
+    }
+]
+    
+    
+   
