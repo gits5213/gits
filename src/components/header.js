4bfeea0ce02a3ab6f671fc90dc05edf682b914ce
md shahnewaz zaman <mzaman@resqsoft.com>
2022-01-12 22:03:01 -0500

    Registration linked changed

--- src/components/header.js
+++ src/components/header.js
@@ -8,15 +8,6 @@ const headerText = () => {
                 <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.SQA_SIGN_UP} target="_blank" rel="noopener noreferrer">
                     SQA-SignUp
                 </a>
-                <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.WEB_DEV_SIGN_UP} target="_blank" rel="noopener noreferrer">
-                    WebDev-SignUp
-                </a>
-                <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.CODE4KIDS_SIGN_UP} target="_blank" rel="noopener noreferrer">
-                    Code4Kids-SignUp
-                </a>
-                <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.BD_SIGN_UP} target="_blank" rel="noopener noreferrer">
-                    BD-SignUp
-                </a>
             </div>
         </div>
         
