ba0107d1d815ac52fd5cba74f6cd0b3062a88c18
md shahnewaz zaman <mzaman@resqsoft.com>
2022-01-12 21:55:05 -0500

    Zoom linked Chnaged

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -21,7 +21,7 @@ class Landing extends Component {
           
                 <div className='ph4 mt4 pt3'> 
                     <a className='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={ZOOM_MEET} target="_blank" rel="noopener noreferrer">
-                        ZOOM Classroom
+                        ZOOM MEETING
                     </a>
                 </div>
                 <div className='pt5 tc ph3 mt4'>
