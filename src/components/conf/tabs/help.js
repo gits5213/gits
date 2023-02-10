02fa8938042e90249af7e150e6191b9570a828ca
md shahnewaz zaman <mzaman@resqsoft.com>
2023-02-10 12:05:14 -0500

    chatGPT added

--- src/components/conf/tabs/help.js
+++ src/components/conf/tabs/help.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleAd from '../../GoogleAd';
 import helpLogo from '../../../images/tabs/helpLogo.png';
+import link from '../../../utilities/links.json';
 
 const help = () => {
     return(
@@ -11,6 +12,17 @@ const help = () => {
             <div className='tc'>
                 <img className='help-logo' src={helpLogo} alt='help Logo'/>
             </div>
+            <section>
+                <div>
+                    <ul>
+                        <li>
+                            <a href={link.chatGPT} target="_blank" rel="noopener noreferrer">
+                                OpenAI - ChatGPT              
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
            
             <GoogleAd slot="1541085932" classNames="page-right-side" />
         </div>
