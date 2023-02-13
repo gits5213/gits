c8ec5b621f0d04b3ff246b5983aaddce9c9fa0c1
md shahnewaz zaman <mzaman@resqsoft.com>
2023-02-13 16:30:18 -0500

    github and PERSONAL_PORTFOLIO

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,12 @@ const headerText = () => {
     return(
         <div>
             <div className='pl4 pb3 sign-up'>
+                <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.PERSONAL_GITHUB} target="_blank" rel="noopener noreferrer">
+                    GitHub
+                </a>
+                <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.PERSONAL_PORTFOLIO} target="_blank" rel="noopener noreferrer">
+                    Portfolio
+                </a>
                 <a class='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib dark-green' href={link.SQA_SIGN_UP} target="_blank" rel="noopener noreferrer">
                     SQA-SignUp
                 </a>
