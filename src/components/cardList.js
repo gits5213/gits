59044e67d544deace4da91535de30e7d4be396c3
md shahnewaz zaman <mzaman@resqsoft.com>
2023-08-21 11:17:22 -0400

    Fixed error

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -9,19 +9,20 @@ import apiAutoTest from '../images/apiAutoTest.png';
 import cypressIO from '../images/cypressIO.png';
 import playwright from '../images/playwright.png';
 
-const COURSES = '/gits/courses';
+const COURSES = '/courses';
 
 const cardList = ({cardDetails}) => {
     return(
         <div className='tc' >
-            <Card img={<a href={COURSES} rel="noopener noreferrer" onclick="window.open(‘/gits/courses’, ‘_self’);"><img alt='codeForkids' src={codeForkids} width='200' height='200'/></a>} id={cardDetails[4].id} courseName={cardDetails[4].courseName} techStack={cardDetails[4].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='webDev' src={webDev} width='200' height='200'/></a>} id={cardDetails[5].id} courseName={cardDetails[5].courseName} techStack={cardDetails[5].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='uiManualTest' src={uiManualTest} width='200' height='200'/></a>} id={cardDetails[0].id} courseName={cardDetails[0].courseName} techStack={cardDetails[0].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='apiManualTest' src={apiManualTest} width='200' height='200'/></a>} id={cardDetails[1].id} courseName={cardDetails[1].courseName} techStack={cardDetails[1].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='uiAutoTest' src={uiAutoTest} width='200' height='200'/></a>} id={cardDetails[2].id} courseName={cardDetails[2].courseName} techStack={cardDetails[2].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='apiAutoTest' src={apiAutoTest} width='200' height='200'/></a>} id={cardDetails[3].id} courseName={cardDetails[3].courseName} techStack={cardDetails[3].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='CypressIO' src={cypressIO} width='200' height='200'/></a>} id={cardDetails[7].id} courseName={cardDetails[7].courseName} techStack={cardDetails[7].techStack}/>
-            <Card img={<a href={COURSES} rel="noopener noreferrer"><img alt='Playwright' src={playwright} width='200' height='200'/></a>} id={cardDetails[8].id} courseName={cardDetails[8].courseName} techStack={cardDetails[8].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer" onclick="window.open(‘/courses’, ‘_self’);"><img alt='codeForkids' src={codeForkids} width='200' height='200'/></a>} id={cardDetails[4].id} courseName={cardDetails[4].courseName} techStack={cardDetails[4].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='webDev' src={webDev} width='200' height='200'/></a>} id={cardDetails[5].id} courseName={cardDetails[5].courseName} techStack={cardDetails[5].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='uiManualTest' src={uiManualTest} width='200' height='200'/></a>} id={cardDetails[0].id} courseName={cardDetails[0].courseName} techStack={cardDetails[0].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='apiManualTest' src={apiManualTest} width='200' height='200'/></a>} id={cardDetails[1].id} courseName={cardDetails[1].courseName} techStack={cardDetails[1].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='uiAutoTest' src={uiAutoTest} width='200' height='200'/></a>} id={cardDetails[2].id} courseName={cardDetails[2].courseName} techStack={cardDetails[2].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='apiAutoTest' src={apiAutoTest} width='200' height='200'/></a>} id={cardDetails[3].id} courseName={cardDetails[3].courseName} techStack={cardDetails[3].techStack}/>
+
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='cypressIO' src={cypressIO} width='200' height='200'/></a>} id={cardDetails[6].id} courseName={cardDetails[6].courseName} techStack={cardDetails[6].techStack}/>
+            <Card img={<a href={COURSES} target="_blank" rel="noopener noreferrer"><img alt='playwright' src={playwright} width='200' height='200'/></a>} id={cardDetails[7].id} courseName={cardDetails[7].courseName} techStack={cardDetails[7].techStack}/>
         </div> 
     );
 }
