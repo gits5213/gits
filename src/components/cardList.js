1bbf75860744c39acf5ce4a9cf14d3e3e069481e
md shahnewaz zaman <mzaman@resqsoft.com>
2025-12-02 16:44:06 -0500

    Update cardList image links to navigate to specific course tabs

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -11,21 +11,19 @@ import cypressIO from '../images/cypressIO.png';
 import playwright from '../images/playwright.png';
 import webAccessibility from '../images/webAccessibility.png';
 
-const COURSES = '/courses';
-
 const cardList = ({cardDetails}) => {
     return(
         <div className='tc' >
-            <Card img={<Link to={COURSES}><img alt='codeForkids' src={codeForkids} width='200' height='200'/></Link>} id={cardDetails[4].id} courseName={cardDetails[4].courseName} techStack={cardDetails[4].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='webDev' src={webDev} width='200' height='200'/></Link>} id={cardDetails[5].id} courseName={cardDetails[5].courseName} techStack={cardDetails[5].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='uiManualTest' src={uiManualTest} width='200' height='200'/></Link>} id={cardDetails[0].id} courseName={cardDetails[0].courseName} techStack={cardDetails[0].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='apiManualTest' src={apiManualTest} width='200' height='200'/></Link>} id={cardDetails[1].id} courseName={cardDetails[1].courseName} techStack={cardDetails[1].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='uiAutoTest' src={uiAutoTest} width='200' height='200'/></Link>} id={cardDetails[2].id} courseName={cardDetails[2].courseName} techStack={cardDetails[2].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='apiAutoTest' src={apiAutoTest} width='200' height='200'/></Link>} id={cardDetails[3].id} courseName={cardDetails[3].courseName} techStack={cardDetails[3].techStack}/>
+            <Card img={<Link to="/courses/code4kids"><img alt='codeForkids' src={codeForkids} width='200' height='200'/></Link>} id={cardDetails[4].id} courseName={cardDetails[4].courseName} techStack={cardDetails[4].techStack}/>
+            <Card img={<Link to="/courses/webdevelopment"><img alt='webDev' src={webDev} width='200' height='200'/></Link>} id={cardDetails[5].id} courseName={cardDetails[5].courseName} techStack={cardDetails[5].techStack}/>
+            <Card img={<Link to="/courses/manual"><img alt='uiManualTest' src={uiManualTest} width='200' height='200'/></Link>} id={cardDetails[0].id} courseName={cardDetails[0].courseName} techStack={cardDetails[0].techStack}/>
+            <Card img={<Link to="/courses/manual"><img alt='apiManualTest' src={apiManualTest} width='200' height='200'/></Link>} id={cardDetails[1].id} courseName={cardDetails[1].courseName} techStack={cardDetails[1].techStack}/>
+            <Card img={<Link to="/courses/automation"><img alt='uiAutoTest' src={uiAutoTest} width='200' height='200'/></Link>} id={cardDetails[2].id} courseName={cardDetails[2].courseName} techStack={cardDetails[2].techStack}/>
+            <Card img={<Link to="/courses/automation"><img alt='apiAutoTest' src={apiAutoTest} width='200' height='200'/></Link>} id={cardDetails[3].id} courseName={cardDetails[3].courseName} techStack={cardDetails[3].techStack}/>
 
-            <Card img={<Link to={COURSES}><img alt='cypressIO' src={cypressIO} width='200' height='200'/></Link>} id={cardDetails[6].id} courseName={cardDetails[6].courseName} techStack={cardDetails[6].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='playwright' src={playwright} width='200' height='200'/></Link>} id={cardDetails[7].id} courseName={cardDetails[7].courseName} techStack={cardDetails[7].techStack}/>
-            <Card img={<Link to={COURSES}><img alt='webAccessibility' src={webAccessibility} width='200' height='200'/></Link>} id={cardDetails[8].id} courseName={cardDetails[8].courseName} techStack={cardDetails[8].techStack}/>
+            <Card img={<Link to="/courses/automation"><img alt='cypressIO' src={cypressIO} width='200' height='200'/></Link>} id={cardDetails[6].id} courseName={cardDetails[6].courseName} techStack={cardDetails[6].techStack}/>
+            <Card img={<Link to="/courses/automation"><img alt='playwright' src={playwright} width='200' height='200'/></Link>} id={cardDetails[7].id} courseName={cardDetails[7].courseName} techStack={cardDetails[7].techStack}/>
+            <Card img={<Link to="/courses/accessibility"><img alt='webAccessibility' src={webAccessibility} width='200' height='200'/></Link>} id={cardDetails[8].id} courseName={cardDetails[8].courseName} techStack={cardDetails[8].techStack}/>
         </div> 
     );
 }
